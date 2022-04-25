9dbbd069e123d1f9aeaa140b98eacd6a518fd4d1
Cecy <ceecysr@gmail.com>
2022-04-24 22:28:39 -0500

    fixed word counter; added img; styled

--- Word Count Tool/app.js
+++ Word Count Tool/app.js
@@ -2,8 +2,22 @@ let button = document.getElementById('btn');
 
 button.addEventListener('click', function(){
     let word = document.getElementById('str').value;
-    let count = word.length;
+    
+    /*split method: The split() method divides a String into an ordered list of substrings, puts these substrings into an array, and returns the array.*/
+    let wordcount = word.split(" ").length;
+
+    //solution is wrong; misses last word bc it doesn't work with newlines
+   /* var counter = 0; 
+   for(var i = 0; i < word.length; i++){
+        if(word[i]==" " || word[i] =="\n"){
+            counter++;
+        }
+    }*/
+
+    //this counts #characters, not words
+    // let count = word.length;
+    
     let outputDiv = document.getElementById('output');
 
-    outputDiv.innerHTML = `<h1>${count}</h1>`
+    outputDiv.innerHTML = `${wordcount}`
 });
